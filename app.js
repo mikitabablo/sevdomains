177ea5e0b782bc8c17049479321a1e3231a39148
mikitabablo <mikitabablo@gmail.com>
2021-02-12 23:04:18 +0300

    Changed port, updated README

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const vhost = require('vhost');
 
 const hostRoutes = require("./api/routes/hosts");
 
